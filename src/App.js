a14ec75409762900e2e447f1c4c3a99581ed68e5
kirandeep123 <me@kiran.cc>
2020-02-18 08:27:31 +0530

    jsx implementation and other tools added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h3", {}, breed)
-  ]);
-};
-
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 const App = () => {
   return React.createElement("div", { id: "something-veryimportant" }, [
     React.createElement("h1", {}, "Adopt me!"),
@@ -26,4 +21,4 @@ const App = () => {
     })
   ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Pet({ name, animal, breed }) {
+  return (
+    <div key="kiran">
+      <h1 key="name">{name}</h1>
+      <h2 key="animal">{animal}</h2>
+      <h3 key="breed">{breed}</h3>
+    </div>
+  );
+}
