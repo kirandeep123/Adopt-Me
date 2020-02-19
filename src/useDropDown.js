52b897e89637be317764e7f93dad0882af1c57f2
kirandeep123 <me@kiran.cc>
2020-02-19 22:44:22 +0530

    dropdown generic hook created

---
+++ src/useDropDown.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+// my custom hooks
+const useDropDown = (label, defaultState, options) => {
+  const [state, setState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const DropDown = () => (
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={e => setState(e.target.value)}
+        disabled={options.length === 0}
+      >
+        <option>All</option>
+        {options.map(option => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+  return [state, DropDown, setState];
+};
+
+export default useDropDown;
